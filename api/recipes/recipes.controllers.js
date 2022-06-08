cf33092f8a7d2b1307e99d215ab0ca2f95d82eab
Msdalmajdi <msdalmajdi@hotmail.com>
2022-06-08 16:51:56 +0300

    Add recipes controlls and routes

---
+++ api/recipes/recipes.controllers.js
@@ -0,0 +1,20 @@
+const Recipe = require("../../models/Recipe");
+
+exports.createRecipe = async (req, res) => {
+  try {
+    const newRecipe = await Recipe.create(req.body);
+    res.status(201).json(newRecipe);
+  } catch (error) {
+    console.log("we caught the error in Recipe Create ", error);
+    res.status(500).json(error);
+  }
+};
+
+exports.getRecipes = async (req, res) => {
+  try {
+    const recipes = await Recipe.find();
+    res.status(201).json(recipes);
+  } catch (err) {
+    res.status(500).json("Server Error");
+  }
+};

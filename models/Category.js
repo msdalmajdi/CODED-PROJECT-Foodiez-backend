6832e2d290b633a2e202ab806b8eebbdf773c834
Msdalmajdi <msdalmajdi@hotmail.com>
2022-06-08 16:17:06 +0300

    edit small typo

--- models/Category.js
+++ models/Category.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const CategorySchema = new mongoose.Schema({
-  name: { type: String, required: true, unqie: true },
+  name: { type: String, required: true, unique: true },
   recipes: [
     {
       type: mongoose.Schema.Types.ObjectId,
